c8527390182eea1d3cfcdce2f1f4c68d6933d7e2
Sveton_Embedded <drenda204@gmail.com>
2026-08-14 22:13:58 +0300

    Practice with Volatile , Temperature sensor 3 time sensor check

--- Volatile.c
+++ Volatile.c
@@ -1,41 +1,55 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-volatile float sensorValue = 0.0f;
-volatile bool dataReady = false;
+volatile float temperature = 0.0f;
+volatile bool newData = false;
 
-void sensorInterrupt(void)
+void sensorInterrupt(float newTemperature)
 {
-    sensorValue = 75.0f;
-    dataReady = true;
+    temperature = newTemperature;
+    newData = true;
 }
 
 int main(void)
 {
+    float testMeasurements[3] = {75.0f, 85.0f, 105.0f};
 
-int waitCount = 0;
-
-    while(!dataReady)
+    for (int i = 0; i < 3; i++)
     {
-       printf("Waiting for sensor...\n"); 
+        int waitCount = 0;
+        while(!newData)
+        {
+            printf("Waiting for sensor...\n");
+            
+            waitCount++;
+            
+            if(waitCount == 3)
+            {
+                sensorInterrupt(testMeasurements[i]);
+            }
+        }
 
-       waitCount++;
+        printf("========================\n");
+        printf("NEW SENSOR DATA\n");
+        printf("========================\n");
+        printf("Temperature: %.1f C\n\n", temperature);
 
-    if(waitCount == 5)
-    {
-        sensorInterrupt();
+        if (temperature < 80.0f)
+        {
+            printf("MOTOR OK\n\n");
+        }
+        else if (temperature < 100.0f)
+        {
+            printf("WARNING\n\n");
+        }
+        else
+        {
+            printf("CRITICAL\n\n");
+        }
+
+        newData = false;
     }
-    }
-
-
-    printf("========================\n");
-    printf("Sensor Data received\n");
-    printf("========================\n");
-    printf("Temperature: %.1f C\n", sensorValue);
 
     return 0;
-
 }
-
-
 
