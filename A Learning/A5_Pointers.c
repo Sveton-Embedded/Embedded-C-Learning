d9d59af4f5ea1ddf3490e15d5d941b155dc21f61
Sveton_Embedded <drenda204@gmail.com>
2026-09-11 09:45:13 +0300

    A5 practice work with Pointers to Pointer

---
+++ A Learning/A5_Pointers.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+void allocate_buffer(int **out, int size)
+{
+    *out = malloc(sizeof(int)*size);
+
+    if (*out == NULL)
+    {
+        printf("Malloc failed!\n");
+    }
+}
+
+int main(void)
+{
+    int value = 10;
+    int *ptr = &value;
+    int **ptr2 = &ptr;
+
+    printf("value = %d\n", value);
+    printf("*ptr = %d\n", *ptr);
+    printf("**ptr2 = %d\n", **ptr2);
+
+    int *arr = NULL;
+    allocate_buffer(&arr, 5);
+
+    if(arr != NULL)
+    {
+        for(int i = 0; i < 5; i++)
+        {
+            arr[i] = i * 10;
+        }
+
+        for (int i = 0; i < 5; i++)
+        {
+            printf("arr[%d] = %d\n", i, arr[i]);
+        }
+
+        free(arr);
+        arr = NULL;
+    }
+
+return 0;
+}
